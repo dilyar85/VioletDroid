ce16be4beb85c51a99687847e77168e5ca2d6245
Dilyar85 <dilyar.safudin@gmail.com>
2016-11-22 11:46:05 -0800

    Added backend service framework.

--- app/src/main/java/com/github/dilyar85/violetdroid/MainActivity.java
+++ app/src/main/java/com/github/dilyar85/violetdroid/MainActivity.java
@@ -14,9 +14,14 @@ import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVFile;
+import com.avos.avoscloud.ProgressCallback;
+import com.avos.avoscloud.SaveCallback;
 import com.github.dilyar85.violetdroid.adapter.RecyclerAdapter;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.Date;
 
@@ -80,7 +85,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
 
         int id = item.getItemId();
-        if (id == R.id.share_item) shareDiagramAsImage();
+        if (id == R.id.menu_share) shareCurrentDiagram();
+        if(id == R.id.menu_save) saveDiagram();
 
         return true;
 
@@ -88,10 +94,37 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    /**
-     * Share the current diagram using any appropriate applications
-     */
-    private void shareDiagramAsImage() {
+    private void saveDiagram() {
+
+        String diagramPth = getCurrentDiagram();
+        if(diagramPth == null) return;
+        try {
+            AVFile file = AVFile.withAbsoluteLocalPath("LeanCloud.png", diagramPth);
+            file.saveInBackground(new SaveCallback() {
+
+                @Override
+                public void done(AVException e) {
+
+                    Log.e(LOG_TAG, "Done!");
+                }
+            }, new ProgressCallback() {
+
+                @Override
+                public void done(Integer integer) {
+
+                    Log.e(LOG_TAG, "Progress: " + integer);
+                }
+            });
+
+        } catch (FileNotFoundException e) {
+            Log.e(LOG_TAG, e.getMessage());
+        }
+
+    }
+
+
+
+    private String getCurrentDiagram() {
 
         ViewGroup canvasView = (ViewGroup) findViewById(R.id.fragment_container);
         canvasView.setDrawingCacheEnabled(true);
@@ -99,27 +132,43 @@ public class MainActivity extends AppCompatActivity {
         try {
             Date now = new Date();
             android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
-            File file = new File(getExternalCacheDir(), now + ".png");
+            File file = new File(getCacheDir(), now + ".png");
+            Log.e(LOG_TAG, "Path: " + file.getAbsolutePath());
             FileOutputStream fOut = new FileOutputStream(file);
             sharedBitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
             fOut.flush();
             fOut.close();
             file.setReadable(true, false);
             canvasView.setDrawingCacheEnabled(false);
-
-            //Create a sharing intent and start it.
-            Intent intent = new Intent();
-            intent.setAction(Intent.ACTION_SEND);
-            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
-            intent.setType("image/*");
-            PackageManager pm = getPackageManager();
-            if(intent.resolveActivity(pm) != null) startActivity(intent);
-            else Toast.makeText(this, getString(R.string.toast_no_intent_applications),Toast.LENGTH_LONG).show();
-
+            return file.getAbsolutePath();
         } catch (Exception e) {
             Log.e(LOG_TAG, e.getMessage());
             Toast.makeText(this, getString(R.string.toast_no_available_space), Toast.LENGTH_LONG).show();
+            return null;
         }
+    }
+
+
+
+    /**
+     * Share the current diagram with any appropriate applications in the device
+     */
+    private void shareCurrentDiagram() {
+
+        String diagramPath = getCurrentDiagram();
+        if(diagramPath == null) return;
+
+        //Create a sharing intent and start it.
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(diagramPath)));
+        intent.setType("image/*");
+        PackageManager pm = getPackageManager();
+        if (intent.resolveActivity(pm) != null) startActivity(intent);
+        else
+            Toast.makeText(this, getString(R.string.toast_no_intent_applications), Toast.LENGTH_LONG).show();
 
     }
+
 }
+
---
+++ app/src/main/java/com/github/dilyar85/violetdroid/application/MyApplication.java
@@ -0,0 +1,19 @@
+package com.github.dilyar85.violetdroid.application;
+import android.app.Application;
+
+import com.avos.avoscloud.AVOSCloud;
+
+/**
+ * Application class
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+        //Initialize LeanCloud service, please do not modify this line.
+        AVOSCloud.initialize(this,"Tmir9DjTPi8gcE83mSclCKAb-MdYXbMMI","sntOwsBRtiBAkBoW3yOWVf7B");
+        AVOSCloud.useAVCloudUS();
+    }
+}
