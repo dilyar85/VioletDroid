32f292c123af685f2d06011abf986ac042cd7b58
Dilyar85 <dilyar.safudin@gmail.com>
2016-11-29 19:41:23 -0800

    Updated exec.sh file

--- exec.sh
+++ exec.sh
@@ -1,4 +1,4 @@
 ./gradlew check -x lint
 ./gradlew assembleDebug
 adb install -r app/build/outputs/apk/app-debug.apk
-adb shell am start -n "com.github.dilyar85.violetdroid/.MainActivity"
+adb shell am start -n "com.github.dilyar85.violetdroid/.SplashActivity"
